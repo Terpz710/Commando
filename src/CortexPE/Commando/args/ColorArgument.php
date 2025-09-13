f258af3cd52e3ca37c06d15eb6bcfe7e2b6b36da
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-09-13 16:49:26 -0700

    Create ColorArgument.php

---
+++ src/CortexPE/Commando/args/ColorArgument.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CortexPE\Commando\args;
+
+use pocketmine\command\CommandSender;
+
+class ColorArgument extends StringEnumArgument {
+
+    protected const VALUES = [
+        "white" => "white",
+        "orange" => "orange",
+        "magenta" => "magenta",
+        "light_blue" => "light_blue",
+        "yellow" => "yellow",
+        "lime" => "lime",
+        "pink" => "pink",
+        "gray" => "gray",
+        "light_gray" => "light_gray",
+        "cyan" => "cyan",
+        "purple" => "purple",
+        "blue" => "blue",
+        "brown" => "brown",
+        "green" => "green",
+        "red" => "red",
+        "black" => "black",
+    ];
+
+    public function getTypeName() : string{
+        return "color";
+    }
+
+    public function getEnumName() : string{
+        return "color";
+    }
+
+    public function parse(string $argument, CommandSender $sender) : string{
+        return $this->getValue($argument);
+    }
+}
